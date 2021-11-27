71472e4a5f7d29179077dd7690e877abfd54a7a8
TELK <telk5093@gmail.com>
2021-11-27 16:34:44 +0900

    Change: Move example js file

--- examples/index.js
+++ examples/index.js
@@ -1,5 +1,5 @@
 const util = require('util');
-const OpenRCT2Lib = require('./OpenRCT2Lib.js');
+const OpenRCT2Lib = require('../OpenRCT2Lib.js');
 
 const scenario_path = './aa.park';
 let file = new OpenRCT2Lib(scenario_path);
